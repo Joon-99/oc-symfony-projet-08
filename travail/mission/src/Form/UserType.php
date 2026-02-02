de8b2e5b309ce98dfe2a49901e54b44047b97584
Joon-99 <dev.joonioh@outlook.com>
2026-02-02 16:55:39 +0100

    mission-corrections-user

--- travail/mission/src/Form/UserType.php
+++ travail/mission/src/Form/UserType.php
@@ -5,9 +5,12 @@ namespace App\Form;
 use App\Entity\User;
 use App\Enum\ContractEnum;
 use App\Form\DataTransformer\StringToContractEnumTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -15,17 +18,25 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('hiredOn', DateType::class, [
+                'label' => "Date d'entrée",
+            ])
             ->add('status', ChoiceType::class, [
                 'choices' => array_combine(ContractEnum::values(), ContractEnum::values()),
                 'placeholder' => 'Choose a contract',
             ])
-            ->add('isActive')
-            ->add('role')
-            ->add('hiredOn')
-            ->add('password')
+            // ->add('isActive')
+            // ->add('role')
+            // ->add('password')
         ;
 
         // convert between submitted string and ContractEnum instance
