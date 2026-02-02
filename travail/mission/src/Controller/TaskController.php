134a6515edf1b077a75f097e8b0cd52fb04722f5
Joon-99 <dev.joonioh@outlook.com>
2026-02-02 14:29:26 +0100

    mission-ajout-new-task

--- travail/mission/src/Controller/TaskController.php
+++ travail/mission/src/Controller/TaskController.php
@@ -6,16 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Task;
+use App\Entity\Project;
 use App\Form\TaskType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
 
 class TaskController extends AbstractController
 {
+    #[Route('/new/{project}', name: 'app_task_new', methods: ['GET', 'POST'])]
     #[Route('/task/{id}/edit', name: 'app_task_edit', methods: ['GET', 'POST'])]
-    public function taskEdit(Task $task, Request $request, EntityManagerInterface $entityManager): Response {
+    public function taskEdit(?Task $task, ?Project $project = null,Request $request, EntityManagerInterface $entityManager): Response {
         if (!$task) {
-            throw $this->createNotFoundException('Tâche introuvable');
+            $task = new Task();
+            if ($project) {
+                $task->setProject($project);
+            } else {
+                throw new BadRequestException('Une tâche doit appartenir à un projet.');
+            }
         }
         $form = $this->createForm(TaskType::class, $task);
         $form->handleRequest($request);
